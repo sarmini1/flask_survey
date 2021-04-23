d93c7308ed5db2fb997b6a2f002cff09f673bccd
Anna Taylor <aetaylor7@outlook.com>
2021-04-22 20:06:15 -0400

    added docstrings

--- app.py
+++ app.py
@@ -15,23 +15,29 @@ debug = DebugToolbarExtension(app)
 # button will be a link that directs the user to endpoint /questions/0
 @app.route('/')
 def render_intro():
+    """ Allows user to click button to start survey """
     return render_template("survey_start.html", survey_title = survey.title, survey_instructions = survey.instructions)
 
 
 # when user goes to /begin endpoint via a POST request
 # redirect user to /questions/<len(responses)>
+# initializes responses in session
 @app.route('/begin', methods=["POST"])
 def redirect_to_question():
+    """ redirects user to first question when they click begin """
     # responses variable will go on to store all of the user's answers to the questions
     session['responses'] = []
     return redirect("/questions/{len(session['responses'])}")
 
 
-# at the /questions/0/1/2/whatever endpoint
-# render the template for the individual question, which
-# displays just that question and its choices
+# checks to make sure we have not finished survey, then
+# renders the template for the individual question, which
+# displays just that question and its choices OR redirects
+# to thank you page.
 @app.route("/questions/{len(session['responses'])}")
 def show_question():
+    """ renders template for next question or redirects to
+     end of survey if finished """
     if len(session['responses']) < len(survey.questions):
         current_question = survey.questions[len(session['responses'])]
         return render_template("question.html", 
@@ -45,6 +51,8 @@ def show_question():
 # response list in the session
 @app.route("/answer", methods=["POST"])
 def store_answer():
+    """ adds user response to question to session[responses]
+        and redirects to next question """
     #breakpoint()
     response = session['responses']
     response.append(request.form["answer"])
@@ -56,4 +64,5 @@ def store_answer():
 # template to tell user thank you for completing survey.
 @app.route("/completion")
 def say_thanks():
+    """ thanks user for completing survey """
     return render_template("completion.html")

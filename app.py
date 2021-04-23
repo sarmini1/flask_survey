b10d787dea093f610f9b1841574c5d3525ef2209
Anna Taylor <aetaylor7@outlook.com>
2021-04-22 20:25:19 -0400

    cleaned up URL and used global const for answers

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-
+ANSWERS_KEY = 'responses'
 
 # when user goes to root route
 # render page that shows show the title of the survey, instructions, and a button to start the survey
@@ -16,7 +16,8 @@ debug = DebugToolbarExtension(app)
 @app.route('/')
 def render_intro():
     """ Allows user to click button to start survey """
-    return render_template("survey_start.html", survey_title = survey.title, survey_instructions = survey.instructions)
+    return render_template("survey_start.html", survey_title = survey.title, 
+                                                survey_instructions = survey.instructions)
 
 
 # when user goes to /begin endpoint via a POST request
@@ -26,23 +27,23 @@ def render_intro():
 def redirect_to_question():
     """ redirects user to first question when they click begin """
     # responses variable will go on to store all of the user's answers to the questions
-    session['responses'] = []
-    return redirect("/questions/{len(session['responses'])}")
+    session[ANSWERS_KEY] = []
+    return redirect(f"/questions/{len(session[ANSWERS_KEY])}")
 
 
 # checks to make sure we have not finished survey, then
 # renders the template for the individual question, which
 # displays just that question and its choices OR redirects
 # to thank you page.
-@app.route("/questions/{len(session['responses'])}")
-def show_question():
+@app.route("/questions/<int:q_id>")
+def show_question(q_id):
     """ renders template for next question or redirects to
      end of survey if finished """
-    if len(session['responses']) < len(survey.questions):
-        current_question = survey.questions[len(session['responses'])]
+    responses = session[ANSWERS_KEY]
+    if len(responses) < len(survey.questions):
+        current_question = survey.questions[len(responses)]
         return render_template("question.html", 
-                                question = current_question, 
-                                choices = current_question.choices)
+                                question = current_question)
     else:
         return redirect("/completion")
 
@@ -54,10 +55,11 @@ def store_answer():
     """ adds user response to question to session[responses]
         and redirects to next question """
     #breakpoint()
-    response = session['responses']
-    response.append(request.form["answer"])
-    session['responses'] = response
-    return redirect("/questions/{len(session['responses'])}")
+    answer = request.form['answer']
+    response = session[ANSWERS_KEY]
+    response.append(answer)
+    session[ANSWERS_KEY] = response
+    return redirect(f"/questions/{len(session[ANSWERS_KEY])}")
 
     
 # makes get request to endpoint of /completion and renders
